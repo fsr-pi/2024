e7808a9586107316871a18a453677048e893fb25
Boris Milašinović <boris.milasinovic@fer.hr>
2024-05-14 08:04:58 +0200

    Primjer za testiranje

---
+++ DefensiveProgramming/Logging/AB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Logging;
+
+public class A
+{
+  public A() { Console.WriteLine("CTOR od A"); }
+}
+
+public class B
+{
+  public B(A a) { Console.WriteLine("CTOR od B"); }
+}
